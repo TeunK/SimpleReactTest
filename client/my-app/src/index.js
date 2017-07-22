b20833a8fd71b4b5be22a99ec6a16bf593b989f8
teun <teunk1@gmail.com>
2017-07-22 09:41:19 +0000

    Added css for students list

--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -264,9 +264,27 @@ class UsersField extends React.Component {
 
 class UsersList extends React.Component {
     render() {
-        const listItems = this.props.users.map((user) =>
-            <li>{user.email}</li>
-        );
+        const header = [{name:"Name", email:"Email"}];
+        const users = this.props.users;
+
+
+
+        const listItems = header.concat(users).map((user, index) => {
+            const highlightHeader = (index === 0) ? "t-b" : "";
+
+            return (
+                <li className="userItem mbs">
+                    <div>
+                        <div className={"w50p float-l " + highlightHeader}>
+                            {user.name}
+                        </div>
+                        <div className={"w50p float-r " + highlightHeader}>
+                            {user.email}
+                        </div>
+                    </div>
+                </li>
+            )
+        });
 
         return (
             <ul>
